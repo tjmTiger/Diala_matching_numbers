ce1cfb75d4fcfb8d171399753b54de9af5ad35cb
tjmTiger <136208644+tjmTiger@users.noreply.github.com>
2025-06-08 01:02:52 +0200

    add working menu

--- gui.py
+++ gui.py
@@ -1,1 +1,48 @@
-print("gh")
+# https://thepythoncode.com/article/make-a-button-using-pygame-in-python#:~:text=Now%20we%20can%20finally%20start,button%20will%20be%20pressed%20once.
+import sys
+import pygame
+
+class Button():
+    def __init__(self, objects, x, y, width, height, buttonText='Button', onclickFunction=None, onePress=False, font = ['Arial', 40]):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.onclickFunction = onclickFunction
+        self.onePress = onePress
+        self.alreadyPressed = False
+
+        self.fillColors = {
+            'normal': '#ffffff',
+            'hover': '#666666',
+            'pressed': '#333333',
+        }
+
+        # surface that contains the buttonText
+        self.buttonSurface = pygame.Surface((self.width, self.height))
+        self.buttonRect = pygame.Rect(self.x, self.y, self.width, self.height)
+        self.buttonSurf = pygame.font.SysFont(font[0], font[1]).render(buttonText, True, (20, 20, 20))
+
+        objects.append(self)
+
+    def process(self, screen):
+        mousePos = pygame.mouse.get_pos()
+        self.buttonSurface.fill(self.fillColors['normal'])
+        if self.buttonRect.collidepoint(mousePos):
+            self.buttonSurface.fill(self.fillColors['hover'])
+            if pygame.mouse.get_pressed(num_buttons=3)[0]:
+                self.buttonSurface.fill(self.fillColors['pressed'])
+                if self.onePress:
+                    self.onclickFunction()
+                elif not self.alreadyPressed:
+                    self.onclickFunction()
+                    self.alreadyPressed = True
+            else:
+                self.alreadyPressed = False
+
+        # blitting the text onto the buttonSurface and then this surface onto the screen
+        self.buttonSurface.blit(self.buttonSurf, [
+            self.buttonRect.width/2 - self.buttonSurf.get_rect().width/2,
+            self.buttonRect.height/2 - self.buttonSurf.get_rect().height/2
+        ])
+        screen.blit(self.buttonSurface, self.buttonRect)
--- main.py
+++ main.py
@@ -20,7 +20,7 @@ print(board)
 ########################
 
 pygame.init()
-screen = pygame.display.set_mode((400, 600))
+screen = pygame.display.set_mode((400, 700))
 fps = 60
 fpsClock = pygame.time.Clock()
 
@@ -28,21 +28,72 @@ pygame.display.set_caption("Matching Numbers")
 program_icon = pygame.image.load('img\icon.png')
 pygame.display.set_icon(program_icon)
 
+##################
+## Window setup ##
+##################
+font = ['Arial', 20]
+
+class Window:
+    def __init__(self):
+        self.objects = []
+    
+    def run(self):
+        for object in self.objects:
+            object.process(screen)
+
+menu_window = Window()
+
+def continue_game():
+    print("Continue button pressed")
+    global window
+    window = "game"
+Button(menu_window.objects, 150, 10, 100, 30, 'Continue', continue_game, True, font = font)
+
+def go_2_settings():
+    print("Settings button pressed")
+    # global window
+    # window = "settings"
+Button(menu_window.objects, 150, 50, 100, 30, 'Settings', go_2_settings, True, font = font)
+
+def exit():
+    global run
+    run = False
+Button(menu_window.objects, 150, 90, 100, 30, 'Exit', exit, font = font)
+
+game_window = Window()
+
+def go_2_menu():
+    print("Menu button pressed")
+    global window
+    window = "menu"
+Button(game_window.objects, 10, 10, 100, 30, 'Menu', go_2_menu, font = font)
+
 #################
 ## Window Loop ##
 #################
 
 run = True
+window = "game"
 while run:
+    screen.fill((0, 0, 0)) # reset canvas
+
     # user imputs
     for event in pygame.event.get():
         if event.type == pygame.QUIT: # close the game
             run = False
-
+    
+    # switch windows
+    if window == "game":
+        game_window.run()
+    elif window == "menu":
+        menu_window.run()
+    else: window = "menu"
     
     # update display window
+    # pygame.display.flip() # pygame.display.update(), but only part of the screen
     pygame.display.update()
 
     # delay
     fpsClock.tick(fps)
 pygame.quit()
+sys.exit()
